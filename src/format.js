3967522d85b6853668f84a81112a4ced10fae7d3
tomiharu0317 <tomiharu1017@gmail.com>
2020-11-23 16:00:45 +0900

    succeeded in showing all reminds

--- src/format.js
+++ src/format.js
@@ -1,44 +1,50 @@
 const arrangeMessageFormat = (text) => {
-
-    return {
-        "type":"text",
-        "text": text
-    };
-}
+  return {
+    type: 'text',
+    text: text,
+  };
+};
 
 const arrangeImageFormat = (originalContetUrl, previewImageUrl) => {
-
-    return {
-        "type": "image",
-        "originalContentUrl": originalContetUrl,
-        "previewImageUrl": previewImageUrl
-    };
-}
+  return {
+    type: 'image',
+    originalContentUrl: originalContetUrl,
+    previewImageUrl: previewImageUrl,
+  };
+};
 
 const arrangeTotalSleepingHoursFormat = (totalSleepingHours) => {
-    let cautionValues = [];
-    let list = totalSleepingHours.split(':');
-    let val  = list[0] + '時間' + list[1] + '分';
-    cautionValues.push([val]);
+  let cautionValues = [];
+  let list = totalSleepingHours.split(':');
+  let val = list[0] + '時間' + list[1] + '分';
+  cautionValues.push([val]);
 
-    return cautionValues;
-}
+  return cautionValues;
+};
 
 const arrangeTimeFormat = (time) => {
   let hour = time.getHours().toString().padStart(2, '0');
   let minite = time.getMinutes().toString().padStart(2, '0');
 
   return hour + ':' + minite;
-}
+};
 
 /**
  * @param {Object} flexMessage
  */
 const arrangeFlexMessageFormat = (flexMessage) => {
+  return {
+    type: 'flex',
+    altText: 'This is a Flex Message',
+    contents: flexMessage,
+  };
+};
 
-    return {
-        "type": "flex",
-        "altText": "This is a Flex Message",
-        "contents": flexMessage
-    };
-}
+const arrangeRemindFormat = (remind) => {
+  return {
+    type: 'text',
+    text: '・' + remind,
+    margin: '10px',
+    wrap: true,
+  };
+};
