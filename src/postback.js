b79880f2da3527f4f6f658b9ca8f754a493972af
tomiharu0317 <tomiharu1017@gmail.com>
2020-11-23 21:25:15 +0900

    succeeded in confirmDate()

--- src/postback.js
+++ src/postback.js
@@ -1,9 +1,103 @@
 /**
  * postback.datetimeを受け取ってその日の睡眠時間を返す
- * @param {String} date
+ * @param {String} date YYYY-MM-DD
+ * @return {Object} flexMessageObject
  */
 const confirmDate = (date) => {
-  return arrangeMessageFormat('確認日付\n' + date);
+  let dateList = date.split('-');
+  let year = dateList[0];
+  let month = dateList[1];
+  let day = Number(dateList[2]);
+  let targetColumn = 2 * day - 1;
+
+  let lastRow = findTargetRow(targetColumn, recordSheet.getLastRow());
+  let recordNum = getRecordNum(lastRow);
+
+  if (recordNum === 0) {
+    return arrangeMessageFormat('記録はありません');
+  }
+
+  let totalSleepingTimes = recordSheet
+    .getRange(lastRow, targetColumn + 1)
+    .getValues();
+  totalSleepingTimes = arrangeRecordedSleepiingTimeFormat(
+    totalSleepingTimes[0][0]
+  );
+
+  let sleepingTimesObj = {
+    type: 'bubble',
+    header: {
+      type: 'box',
+      layout: 'vertical',
+      contents: [
+        {
+          type: 'text',
+          text: year + '年' + month + '月' + day.toString() + '日の睡眠',
+          weight: 'bold',
+          size: '18px',
+        },
+      ],
+      height: '70px',
+    },
+    hero: {
+      type: 'box',
+      layout: 'vertical',
+      contents: [],
+    },
+    body: {
+      type: 'box',
+      layout: 'vertical',
+      contents: [],
+    },
+    footer: {
+      type: 'box',
+      layout: 'horizontal',
+      contents: [
+        {
+          type: 'text',
+          text: '合計睡眠時間',
+          weight: 'bold',
+          size: '14px',
+          margin: '10px',
+        },
+        {
+          type: 'text',
+          text: totalSleepingTimes,
+          weight: 'bold',
+        },
+      ],
+    },
+  };
+
+  sleepingTimesObj = addSleepingTimesToObj(
+    sleepingTimesObj,
+    recordNum,
+    targetColumn
+  );
+
+  return arrangeFlexMessageFormat(sleepingTimesObj);
+};
+
+/**
+ * それぞれの[就寝、起床、睡眠時間]をflexMessageのObjに追加する
+ * @param {Obj} sleepingTimesObj
+ * @param {Number} recordNum
+ * @param {Number} targetColumn
+ */
+const addSleepingTimesToObj = (sleepingTimesObj, recordNum, targetColumn) => {
+  let sleepRecord;
+
+  for (let recordIndex = 1; recordIndex < recordNum + 1; recordIndex++) {
+    // [['就寝', 'date'], ['起床', 'date'], ['睡眠時間', 'date']];
+    sleepRecord = recordSheet
+      .getRange(4 * recordIndex - 2, targetColumn, 3, 2)
+      .getValues();
+
+    sleepRecord = arrangeSleepingRecordToObj(sleepRecord, recordIndex);
+    sleepingTimesObj.body.contents.push(sleepRecord);
+  }
+
+  return sleepingTimesObj;
 };
 
 /**
